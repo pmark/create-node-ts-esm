4bc42507d52e0b6b6422211c5e569b12f6e03b1d
P. Mark Anderson <mark.anderson@cirium.com>
2024-03-16 15:23:13 -0700

    v1.0.2

--- template/jest.config.js
+++ template/jest.config.js
@@ -1,16 +1,9 @@
-// import { pathsToModuleNameMapper } from 'ts-jest/utils';
-// import { compilerOptions } from './tsconfig.json';
-
 export default {
-  preset: 'ts-jest/presets/default-esm', // Use the ESM preset of ts-jest
+  preset: 'ts-jest/presets/default-esm',
   globals: {},
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1', // Resolve .js extensions to .ts files
   },
-  // moduleNameMapper: {
-  //   ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }), // Adjust if you're using path mapping in your tsconfig
-  //   '^(\\.{1,2}/.*)\\.js$': '$1', // Handle .js extensions for ESM
-  // },
   transform: {
     '^.+\\.ts$': [
       'ts-jest',
@@ -21,26 +14,7 @@ export default {
   },
   extensionsToTreatAsEsm: ['.ts'],
   testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'js', 'json'], // Specify file extensions
-  testMatch: ['<rootDir>/__tests__/**/*.test.ts'], // Adjust if your test files have a different pattern
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
 };
 
-
-
-const default2 = {
-  displayName: 'my-app-tests',
-  testMatch: ['**/?(*.)+(integration).(test).ts'],
-  globals: {},
-  preset: 'ts-jest/presets/default-esm', // for ESM support
-  extensionsToTreatAsEsm: ['.ts'], // for ESM support
-  testEnvironment: 'node',
-  transform: {}, // keep blank for ESM
-  moduleFileExtensions: ['ts', 'js', 'html', 'node'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1', // Resolve .js extensions to .ts files
-  },
-  transformIgnorePatterns: [
-    "node_modules/",
-    "dist/"
-  ],
-};
